ddcea7417c01f210615b091706ba5c191e47f000
Andrii V. Sukhorukov <andrii@iac.es>
2022-10-24 18:43:40 +0100

    Update BST sources for Lecture 07 classwork

--- Lecture_07/classwork/binary_search_tree/bst_test.f90
+++ Lecture_07/classwork/binary_search_tree/bst_test.f90
@@ -1,8 +1,3 @@
-! Compile either using -DNONRECURSIVE or -DRECURSIVE.  For example,
-!
-!   $ gfortran -g -O0 -Wall -Wextra -Wpedantic -fbacktrace -fcheck=all \
-!              -DNONRECURSIVE bst_mod.F90 bst_test.f90
-!
 program bst_test
     use :: bst_mod
     implicit none
@@ -17,5 +12,32 @@ program bst_test
     end do
     print *
     call bst_display( root )
+
+    block
+        type(a_bst_node), pointer :: temp
+        print "(a)", "Enter values to search for (one per line):"
+        do
+            read(*, *, iostat=iostatus) val
+            if (iostatus /= 0) exit
+            temp => bst_search( root, val )
+            if (associated( temp )) then
+                print "(a, i0)", "temp%val = ", temp%val
+            else
+                print "(a, i0, a)", "Value ", val, " is not in the tree"
+            end if
+        end do
+    end block
+
+    block
+        type(a_bst_node), pointer :: root, temp
+
+        temp => bst_search( root, 6 )
+        print "(a, i0)", "Full sum = ", bst_sum( temp )
+        temp => bst_search( root, 15 )
+        print "(a, i0)", "Partial sum at node 15 = ", bst_sum( temp )
+        temp => bst_search( root, 9 )
+        print "(a, i0)", "Partial sum at non-existing node = ", bst_sum( temp )
+    end block
+
     call bst_destroy( root )
 end program bst_test
